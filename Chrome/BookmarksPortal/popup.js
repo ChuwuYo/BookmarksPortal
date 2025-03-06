5b1160b29b15263541f60b1f653b29b433b521c5
HatsuChuwu <141227996+HatsuChuwu@users.noreply.github.com>
2025-03-06 21:29:29 +0800

    ⏰ 修改导出文件命名规则

--- Chrome/BookmarksPortal/popup.js
+++ Chrome/BookmarksPortal/popup.js
@@ -288,7 +288,7 @@ async function exportSelectedBookmarks() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `bookmarks-${new Date().toISOString().split('T')[0]}.json`;
+    a.download = `bookmarks⏰${new Date().toISOString().split('T')[0]}.json`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
